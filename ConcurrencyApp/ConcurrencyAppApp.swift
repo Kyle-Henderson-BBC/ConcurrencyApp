9b65525b59f7b5a9f5eb842e37924e09eff4b1e6
Kyle H <kyle.henderson@bbc.co.uk>
2024-08-23 11:55:12 +0100

    Initial Commit

---
+++ ConcurrencyApp/ConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyAppApp.swift
+//  ConcurrencyApp
+//
+//  Created by Kyle O Henderson on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyAppTests/ConcurrencyAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConcurrencyAppTests.swift
+//  ConcurrencyAppTests
+//
+//  Created by Kyle O Henderson on 23/08/2024.
+//
+
+import Testing
+
+struct ConcurrencyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
